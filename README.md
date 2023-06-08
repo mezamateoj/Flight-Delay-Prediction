c906e981a9a0e6985e873d7637b8dfd698ec8219
Mateo Meza <mezamateoj@gmail.com>
2023-06-08 19:04:07 -0400

    Add clean data

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Trainee-Challenge
 Project for the Trainee Challenge - LATAM Airlines
+
+### Dataset
+There are 22 columns for the dataset and corresponding column description are given below.  
+    
+1. Day of Month
+2. Day of Week starting from Monday
+3. Unique Carrier Code. 
+4. An identification number assigned by US DOT to identify a unique airline (carrier). 
+5. Code assigned by IATA and commonly used to identify a carrier. 
+6. Tail Number
+7. Flight Number
+8. Origin Airport, Airport ID. An identification number assigned by US DOT to identify a unique airport.
+9. Origin Airport, Airport Sequence ID. 
+10. Origin Airport
+11. Destination Airport, Airport ID. An identification number assigned by US DOT to identify a unique airport.
+12. Destination Airport, Airport Sequence ID. 
+13. Destination Airport.
+14. Actual Departure Time (local time: hhmm)
+15. Departure Delay Indicator, 15 Minutes or More (1=Yes, 0=No)
+16. Departure Time Block, Hourly Intervals
+17. Actual Arrival Time (local time: hhmm)
+18. Arrival Delay Indicator, 15 Minutes or More (1=Yes, 0=No)
+19. Cancelled Flight Indicator (1=Yes, 0=No)
+20. Diverted Flight Indicator (1=Yes, 0=No)
+21. Distance between airports (miles)  
+22. Unnamed: 21 - no description available  
--- processing.py
+++ processing.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import zipfile
 
+# data source: https://www.kaggle.com/datasets/divyansh22/flight-delay-prediction?select=Jan_2020_ontime.csv
 # code implementation to ...
 # important link
 # https://github.com/RobMulla/twitch-stream-projects/blob/main/050-flights/DataPrep.ipynb
@@ -9,18 +10,37 @@ import zipfile
 # Define the directory
 directory = 'D:\Trainee-Challenge\datasets'
 
-df_list= []
-# Traverse through the directory
-for filename in os.listdir(directory):
-    if 'csv' not in filename:
-        # Construct the full file path
-        filepath = os.path.join(directory, filename)
-        with zipfile.ZipFile(filepath, 'r') as zfiles:
-            for name in zfiles.namelist():
-                if name.endswith('.csv'):
-                    df = pd.read_csv(zfiles.open(name))
-                    df_list.append(df)
+def concat_data(dir):
+    df_list= []
+    # Traverse through the directory
+    for filename in os.listdir(directory):
+        if 'csv' not in filename:
+            # Construct the full file path
+            filepath = os.path.join(directory, filename)
+            with zipfile.ZipFile(filepath, 'r') as zfiles:
+                for name in zfiles.namelist():
+                    if name.endswith('.csv'):
+                        df = pd.read_csv(zfiles.open(name))
+                        df_list.append(df)
 
-    
-final_df = pd.concat(df_list)
-final_df.to_parquet('./datasets/final.parquet', engine='fastparquet')
+        
+    final_df = pd.concat(df_list)
+    final_df.to_parquet('./datasets/final.parquet', engine='fastparquet')
+
+concat_data(directory)
+
+# air carrier names and codes data.
+
+url = 'https://aspm.faa.gov/aspmhelp/index/ASQP__Carrier_Codes_and_Names.html'
+
+def get_carrier_data(url):
+    #the read_html method recieves the url as a parameter
+    web_tables = pd.read_html(url)
+
+    #let's read the first table on the webpage with index 0
+    carrier_table = web_tables[0]
+    carrier_table.to_csv('./datasets/carrier_table.csv')
+
+get_carrier_data(url)
+
+
